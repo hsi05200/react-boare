067bba3305d958c1408bdc60a3e1c7ecf8c53617
stchwang <hsi05200@gmail.com>
2020-12-29 03:37:28 +0900

    React bootstrap 및 웹폰드(Noto Sans Kr) 사용환경설정완료

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { HashRouter } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Header from './Header';
 import Body from './Body';
 import Footer from "./Footer";
 
 ReactDOM.render(
-  <div>
+  <HashRouter>
     <Header />
     <Body />
     <Footer />
-  </div>,
+  </HashRouter>,
   document.querySelector('#container')
 );
