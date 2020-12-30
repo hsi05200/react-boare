a6ed247b4e54167934bd03c7f928892b68f95fc7
stchwang <hsi05200@gmail.com>
2020-12-31 01:59:25 +0900

    기본 로그인폼 및 회원가입폼 세팅

--- client/src/Body.js
+++ client/src/Body.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import './Body.css';
+import React, { Component } from "react";
+import LoginForm from "./LoginForm";
+import BoardForm from "./BoardForm";
+import BoardWriteForm from "./BoardWriteForm";
+import BoardDetail from "./BoardDetail";
+import MypageForm from "./MypageForm";
+import { Route } from "react-router-dom";
+import $ from "jquery";
+import {} from "jquery.cookie";
 
-class Body extends React.Component {
-
-  render () {
-
+class Body extends Component {
+  render() {
+    let resultForm;
+    function getResultForm() {
+      // console.log($.cookie("login_id"));
+      if ($.cookie("login_id")) {
+        resultForm = <Route exact path="/" component={BoardForm}></Route>;
+        return resultForm;
+      } else {
+        resultForm = <Route exact path="/" component={LoginForm}></Route>;
+        return resultForm;
+      }
+    }
+    getResultForm();
     return (
-      <div className="Body">      
-        <p>여기가 본문입니다.</p>      
+      <div>
+        <Route path="/mypage" component={MypageForm}></Route>
+        <Route path="/boardWrite" component={BoardWriteForm}></Route>
+        <Route path="/board/detail" component={BoardDetail}></Route>
+        {resultForm}
       </div>
     );
   }
--- schemas/board.js
+++ schemas/board.js
@@ -1,11 +1,11 @@
+  
 const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 const {
   Types: { ObjectId }
 } = Schema;
-
-const userSchema = new Schema({
+const boardSchema = new Schema({
   writer: {
     type: ObjectId,
     required: true,
@@ -16,8 +16,11 @@ const userSchema = new Schema({
     required: true
   },
   content: {
-    ttype: String,
+    type: String,
     required: true
+  },
+  imgPath: {
+    type: String
   },
   createdAt: {
     type: Date,
